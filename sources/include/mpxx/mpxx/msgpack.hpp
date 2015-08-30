0310a83dea5c7382705e77afb186cc3a494ec563
Remy Chibois <remy.chibois@expandium.com>
2015-08-30 10:59:43 +0200

    fixing pkgsrc build

--- sources/include/mpxx/mpxx/msgpack.hpp
+++ sources/include/mpxx/mpxx/msgpack.hpp
@@ -4,6 +4,7 @@
 // Get rid of annoying unsued function declared in msgpach.hpp
 #pragma GCC diagnostic ignored "-Wunused-function"
 
+#include <limits.h>
 #include <msgpack.hpp>
 
 #pragma GCC diagnostic pop
